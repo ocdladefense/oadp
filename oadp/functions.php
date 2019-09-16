2ee3038da1e7acc2abda7c21e7f35642d5cf6d37
jack <brainard@my.lanecc.edu>
2019-09-16 11:05:32 -0700

    commit required before pulling

--- oadp/functions.php
+++ oadp/functions.php
@@ -1,6 +1,7 @@
 <?php
 
 function my_theme_enqueue_styles() {
+
     wp_enqueue_style('style', get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'style',
     get_stylesheet_directory_uri() . '/style.css',
