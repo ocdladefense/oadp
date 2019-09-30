1e26396c441fbef3a7093e279c0fe25c42664044
Jose Bernal <jbernal.web.dev@gmail.com>
2019-09-30 01:53:36 +0000

    Clarify naming for stylesheets and load order.

--- functions.php
+++ functions.php
@@ -1,15 +1,46 @@
 <?php
 
-function my_theme_enqueue_styles() {
-    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'custom-styling', get_template_directory_uri() . '/style-overrides.css' );
-	wp_enqueue_style( 'custom-post-styling', get_template_directory_uri() . '/style-overrides-post.css' );
-	wp_enqueue_style( 'custom-header-styling', get_template_directory_uri() . '/style-overrides-header.css' );
-	wp_enqueue_style( 'custom-menu-styling', get_template_directory_uri() . '/style-overrides-menus.css' );
-    wp_enqueue_style( 'custom-footer-styling', get_template_directory_uri() . '/style-overrides-footer.css' );
-    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css');
+/**
+  * @function add_parent_stylesheets
+  *
+  * @description Queue up stylesheets for the parent theme.
+  */
+function add_parent_stylesheets() {
 
+	$basedir = get_template_directory_uri() .'/style-overrides';
+	$styles = array(
+		'general-overrides' => 'style.css',
+		'header-overrides' => 'header.css',
+		'menu-overrides' => 'menu.css',
+		'post-overrides' => 'post.css',
+		'footer-overrides' => 'footer.css'
+	);
+	
+	wp_enqueue_style( 'parent-styles', get_template_directory_uri() . '/style.css' );
+	
+	foreach($styles as $id => $uri) {
+		wp_enqueue_style($id,$basedir.'/'.$uri);
+	}
+	
 }
 
-add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+
+
+/**
+  * @function add_parent_stylesheets
+  *
+  * @description Queue up stylesheets for this theme.
+  */
+function add_child_stylesheets() {
+		
+    wp_enqueue_style( 'child-styles',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( 'parent-styles' ),
+        wp_get_theme()->get('Version')
+    );
+}
+
+
+add_action( 'wp_enqueue_scripts', 'add_parent_stylesheets' );
+add_action( 'wp_enqueue_scripts', 'add_child_stylesheets' );
 
