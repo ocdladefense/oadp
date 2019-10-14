4dc728b7de85367e632b2156ed6141bf8eacb0c4
Jose Bernal <jbernal.web.dev@gmail.com>
2019-10-14 04:23:00 +0000

    Upgrade child theme.

--- functions.php
+++ functions.php
@@ -1,61 +1,65 @@
 <?php
 
-
+// Comment this line to use the parent theme's widget areas.
+// add_action( 'widgets_init', 'override_parent_widget_areas' );
 add_action( 'wp_enqueue_scripts', 'init_transparent_header' );
-add_action( 'wp_enqueue_scripts', 'add_parent_stylesheets' );
-add_action( 'wp_enqueue_scripts', 'add_child_stylesheets' );
-
-
-
-
-
 
 
 
 
 /**
-  * @function add_parent_stylesheets
-  *
-  * @description Queue up stylesheets for the parent theme.
-  */
-function add_parent_stylesheets() {
-
-	$basedir = get_template_directory_uri() .'/style-overrides';
-	$styles = array(
-		'general-overrides' => 'style.css',
-		'header-overrides' => 'header.css',
-		'menu-overrides' => 'menu.css',
-		'post-overrides' => 'post.css',
-		'footer-overrides' => 'footer.css'
-	);
-	
-	wp_enqueue_style( 'parent-styles', get_template_directory_uri() . '/style.css' );
-	
-	foreach($styles as $id => $uri) {
-		wp_enqueue_style($id,$basedir.'/'.$uri);
-	}
-	
-}
-
-
-
-/**
-  * @function add_parent_stylesheets
+  * @function add_child_stylesheets
   *
   * @description Queue up stylesheets for this theme.
   */
 function add_child_stylesheets() {
+	$basedir = get_stylesheet_directory_uri()."/styles";
+	
+	$styles = array(
+		'child.structure' => 'structure.css',
+		'child.header' => 'header.css',
+		'child.image' => 'images.css',
+		'child.menu' => 'menu.css',
+		'child.post' => 'post.css',
+		'child.footer' => 'footer.css',
+		'child.homepage' => 'home.css',
+		'child.widescreen' => 'widescreen.css',
+		'child.sidebar' => 'sidebar.css',
+		'child.widget' => 'widget.css',
+	);
+	$styles = array('child.home'=>'home.css');
+	
+	/*wp_enqueue_style(
+		'child-styles',
+		get_stylesheet_directory_uri() . '/style.css',
+		array( 'parent-styles','menu'),
+		wp_get_theme()->get('Version')
+	);
+	*/
+    
+	foreach($styles as $id => $uri) {
+		$dep = explode('.',$id);//Setup dependency with parent theme stylesheet
 		
-    wp_enqueue_style( 'child-styles',
-        get_stylesheet_directory_uri() . '/style.css',
-        array( 'parent-styles' ),
-        wp_get_theme()->get('Version')
-    );
+		wp_enqueue_style(
+			$id,
+			$basedir.'/'.$uri,
+			array($dep[1]),
+			wp_get_theme()->get('Version')
+		);
+	}
 }
+
+
+
 
 function init_transparent_header() {
 	wp_enqueue_script( 'global-header', get_stylesheet_directory_uri() . '/assets/js/home.js', array('jquery'), true );
 }
 
 
-
+/**
+ * Register widget area.
+ *
+ * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
+ */
+// function override_parent_widget_areas() {}
